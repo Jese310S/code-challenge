61b482ae2711ee319cc95b7a47e611885ae9ff2b
Jese310S <59402195+Jese310S@users.noreply.github.com>
2021-05-04 11:04:42 +1000

    install npm and axios and test get request

---
+++ app.js
@@ -0,0 +1,7 @@
+
+const url = 'jsole.zendesk.com/api/v2/groups.json'
+const key = "VQJdyvA9ADAe1xIzQPZwGknXrh4HHnaMiuzOTJgE"
+axios.get('http://webcode.me').then(resp => {
+
+    console.log(resp.data);
+});
